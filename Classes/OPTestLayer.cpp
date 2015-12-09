1b9d4bbc32bf0a9ae3914c36854e9609c98e7fb9
wutao <116729837@qq.com>
2015-12-09 13:53:03 +0800

    解决按钮点击初始化

--- Classes/OPTestLayer.cpp
+++ Classes/OPTestLayer.cpp
@@ -5,5 +5,64 @@
 //  Created by 涛 吴 on 15/12/8.
 //
 //
-
+#include <iostream>
 #include "OPTestLayer.h"
+USING_NS_CC;
+USING_NS_CC_EXT;
+
+bool OPTestLayer::onAssignCCBMemberVariable(Ref * pTarget, const char * pMemberVariableName, Node * pNode) {
+    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "helloLabel", Label *, this->helloLabel);
+    std::cout<<"hello"<<std::endl;
+    return false;
+}
+
+SEL_MenuHandler OPTestLayer::onResolveCCBCCMenuItemSelector(Ref * pTarget, const char * pSelectorName) {
+    CCB_SELECTORRESOLVER_CCMENUITEM_GLUE(this, "onPressMenuItem", OPTestLayer::onPressButton);
+    return nullptr;
+}
+
+Control::Handler OPTestLayer::onResolveCCBCCControlSelector(Ref * pTarget, const char * pSelectorName) {
+    CCB_SELECTORRESOLVER_CCCONTROL_GLUE(this, "onPressButton", OPTestLayer::onControlButtonClicked);
+    
+    return nullptr;
+}
+
+void OPTestLayer::onControlButtonClicked(cocos2d::Ref *pSender, Control::EventType pControlEvent) {
+    switch(pControlEvent) {
+        case Control::EventType::TOUCH_DOWN:
+            this->helloLabel->setString("Touch Down.");
+            break;
+        case Control::EventType::DRAG_INSIDE:
+            this->helloLabel->setString("Touch Drag Inside.");
+            break;
+        case Control::EventType::DRAG_OUTSIDE:
+            this->helloLabel->setString("Touch Drag Outside.");
+            break;
+        case Control::EventType::DRAG_ENTER:
+            this->helloLabel->setString("Touch Drag Enter.");
+            break;
+        case Control::EventType::DRAG_EXIT:
+            this->helloLabel->setString("Touch Drag Exit.");
+            break;
+        case Control::EventType::TOUCH_UP_INSIDE:
+            this->helloLabel->setString("Touch Up Inside.");
+            break;
+        case Control::EventType::TOUCH_UP_OUTSIDE:
+            this->helloLabel->setString("Touch Up Outside.");
+            break;
+        case Control::EventType::TOUCH_CANCEL:
+            this->helloLabel->setString("Touch Cancel.");
+            break;
+        case Control::EventType::VALUE_CHANGED:
+            this->helloLabel->setString("Value Changed.");
+            break;
+        default:
+            assert(false); // OH SHIT!
+    }
+}
+
+void OPTestLayer::onPressButton(cocos2d::Ref * sender)
+{
+    std::cout<<"hello"<<std::endl;
+    this->helloLabel->setString("1111111");
+}
--- Classes/OPTestLayer.h
+++ Classes/OPTestLayer.h
@@ -13,8 +13,19 @@
 #include "extensions/cocos-ext.h"
 #include "cocosbuilder/CocosBuilder.h"
 
-class OPTestLayer : public cocos2d::Layer {
-    public:
-        CCB_STATIC_NEW_AUTORELEASE_OBJECT_WITH_INIT_METHOD(OPTestLayer, create);
+class OPTestLayer
+    : public cocos2d::Layer
+    , public cocosbuilder::CCBMemberVariableAssigner
+    , public cocosbuilder::CCBSelectorResolver
+{
+public:
+    CCB_STATIC_NEW_AUTORELEASE_OBJECT_WITH_INIT_METHOD(OPTestLayer, create);
+    virtual bool onAssignCCBMemberVariable(cocos2d::Ref * pTarget, const char * pMemberVariableName, cocos2d::Node * node);
+    virtual cocos2d::SEL_MenuHandler onResolveCCBCCMenuItemSelector(cocos2d::Ref * pTarget, const char * pSelectorName);
+    virtual cocos2d::extension::Control::Handler onResolveCCBCCControlSelector(cocos2d::Ref * pTarget, const char * pSelectorName);
+    void onPressButton(cocos2d::Ref * sender);
+    void onControlButtonClicked(cocos2d::Ref * sender, cocos2d::extension::Control::EventType pControlEvent);
+private:
+    cocos2d::Label *helloLabel;
 };
 #endif /* OPTestLayer_hpp */
