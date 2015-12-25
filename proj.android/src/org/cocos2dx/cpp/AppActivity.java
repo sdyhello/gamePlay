239c6c7e06a2b6459ec0bf1b201f8e736f01958c
tao.wu <tao.wu@zentertain.net>
2015-12-25 17:40:48 +0800

    abmob android ok

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -31,29 +31,68 @@ import org.cocos2dx.lib.Cocos2dxActivity;
 import com.tendcloud.tenddata.TalkingDataGA;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
 
 public class AppActivity extends Cocos2dxActivity {
+	private AdView adView;
+	private static final String AD_UNIT_ID = "ca-app-pub-5009665577000890/9760595766";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		TalkingDataGA.sPlatformType=TalkingDataGA.PLATFORM_TYPE_COCOS2DX;
 		TalkingDataGA.init(this.getApplicationContext(), "400D7D1065BEABD0019F98038AD98627", "TalkingData");
+
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.BANNER);
+		adView.setAdUnitId(AD_UNIT_ID);
+
+		AdRequest adRequest = new AdRequest.Builder()
+				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+				.addTestDevice("1DF047946C45C68B7DBD7BB4027CC800").build();
+
+		adView.loadAd(adRequest);
+		adView.setBackgroundColor(Color.BLACK);
+		adView.setBackgroundColor(0);
+
+		FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		params.gravity = Gravity.TOP;
+
+		addContentView(adView, params);
 	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
 		TalkingDataGA.onPause(this);
+		if (adView != null) {
+			adView.pause();
+		}
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
 		TalkingDataGA.onResume(this);
+		if (adView != null) {
+			adView.resume();
+		}
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
+		adView.destroy();
 	}
 }
